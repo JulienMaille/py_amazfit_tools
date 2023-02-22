5a5cb282e02c38eefcfe62c1bacf902c9a5764e7
Julien Maille <julien.maille@gmail.com>
2023-02-22 14:18:21 +0100

    added missing AirQualityProgressElement

--- watchFaceParser/elements/gts2mini/weatherElements/airquality.py
+++ watchFaceParser/elements/gts2mini/weatherElements/airquality.py
@@ -1,9 +1,14 @@
 from watchFaceParser.elements.gts2mini.basicElements.number import Number
 from watchFaceParser.elements.gts2mini.basicElements.image import Image
+from watchFaceParser.elements.gts2mini.basicElements.imageSet import ImageSet
 
 class AirQuality:
     definitions = {
         1: {'Name': 'AirQualityNumber', 'Type': Number},
+        2: {'Name': 'IconProgress', 'Type': ImageSet}, #copied from
         3: {'Name': 'AirQualityIcon', 'Type': Image},
+        4: {'Name': 'Icon', 'Type': Image},
+        5: {'Name': 'IconChinese', 'Type': Image},
+        6: {'Name': 'IconTradChinese', 'Type': Image},
     }
 
