456e98192712846ee7d1c9c0eb8c9aff472b6784
Nikolas Maletschkin <tnxec2@gmail.com>
2023-05-17 11:07:53 +0200

    batterytext correct fileds

--- watchFaceParser/elements/gts2mini/basicElements/batteryText.py
+++ watchFaceParser/elements/gts2mini/basicElements/batteryText.py
@@ -2,12 +2,12 @@
 from watchFaceParser.elements.gts2mini.basicElements.image import Image
 from watchFaceParser.elements.gts2mini.shortcutsElements.element import Element
 
-class Text:
+class BatteryText:
     definitions = {
         1: {'Name': 'ImageNumber', 'Type': Number},
-        2: {'Name': 'PrefixImageIndex', 'Type': 'long'},
-        3: {'Name': 'NoDataImageIndex', 'Type': 'long'},
-        4: {'Name': 'SuffixImageIndex', 'Type': 'long'},
+        2: {'Name': 'UnknownLong2', 'Type': 'long'}, # TODO: prüfen
+        3: {'Name': 'PrefixImageIndex', 'Type': 'long'}, # zepp: 164024822b00d28891e2061e418fc1a4
+        4: {'Name': 'SuffixImageIndex', 'Type': 'long'}, # zepp: 164024822b00d28891e2061e418fc1a4
         5: {'Name': 'Icon', 'Type': Image},
         6: {'Name': 'Shortcut', 'Type': Element},
     }
--- watchFaceParser/elements/gts2mini/battery.py
+++ watchFaceParser/elements/gts2mini/battery.py
@@ -1,4 +1,4 @@
-from watchFaceParser.elements.gts2mini.basicElements.text import Text
+from watchFaceParser.elements.gts2mini.basicElements.batteryText import BatteryText
 from watchFaceParser.elements.gts2mini.basicElements.imageSet import ImageSet
 from watchFaceParser.elements.gts2mini.basicElements.iconSet import IconSet
 from watchFaceParser.elements.gts2mini.basicElements.scale import Scale
@@ -6,7 +6,7 @@ from watchFaceParser.elements.gts2mini.basicElements.image import Image
 
 class Battery:
     definitions = {
-        1: {'Name': 'BatteryText', 'Type': Text},
+        1: {'Name': 'BatteryText', 'Type': BatteryText},
         2: {'Name': 'ImageProgress', 'Type': ImageSet},
         3: {'Name': 'IconSetProgress', 'Type': IconSet},
         #4: {'Name': 'Unknown4', 'Type': 'long?'},
