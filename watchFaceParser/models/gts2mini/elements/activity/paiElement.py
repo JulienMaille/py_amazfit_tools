1eaa639a8e08ce2d7d3f1d8fa7bb52728bada915
Nikolas Maletschkin <tnxec2@gmail.com>
2023-07-04 13:26:01 +0200

    pai _suffix

--- watchFaceParser/models/gts2mini/elements/activity/paiElement.py
+++ watchFaceParser/models/gts2mini/elements/activity/paiElement.py
@@ -7,6 +7,7 @@ class PaiElement(CompositeElement):
         self._image_number = None
         self._nodata = None
         self._icon = None
+        self._suffix = None
         super(PaiElement, self).__init__(parameters = None, parameter = parameter, parent = parent, name = name)
 
     def draw3(self, drawer, resources, state):
@@ -18,7 +19,8 @@ class PaiElement(CompositeElement):
                                      state.getPai(),
                                      minimum_digits= 3,
                                      force_padding = False,
-                                     followxy = None)
+                                     followxy = None,
+                                     suffix=self._suffix)
 
     def createChildForParameter(self, parameter):
         parameterId = parameter.getId()
@@ -29,8 +31,8 @@ class PaiElement(CompositeElement):
             return self._image_number
         elif parameterId == 2:
             from watchFaceParser.models.gts2mini.elements.basic.valueElement import ValueElement
-            self._nodata = parameter.getValue()
-            return ValueElement(parameter, self, 'NoDataImageIndex')
+            self._suffix = parameter.getValue()
+            return ValueElement(parameter, self, 'SuffixImageIndex')
         elif parameterId == 3:
             from watchFaceParser.models.gts2mini.elements.common.imageElement import ImageElement
             self._icon = ImageElement(parameter=parameter, parent=self, name='Icon')
